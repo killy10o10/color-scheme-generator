01af672335c7e934f45f7a9797d14e0f627d6e1e
killy10o10 <89058561+killy10o10@users.noreply.github.com>
2022-08-04 16:51:28 +0000

    Generate  Color from color picker

--- app.js
+++ app.js
@@ -2,20 +2,23 @@ const form = document.querySelector('form');
 const colorScheme = document.querySelector('select');
 const colorCodes = document.querySelector('.color-names');
 const colorBar = [...document.querySelectorAll('.color-bar')];
-const baseURL = 'https://www.thecolorapi.com/scheme';
+const baseURL = 'https://www.thecolorapi.com';
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  console.log(colorBar);
-});
+  const colorPicker = document.getElementById('color-picker').value;
+  const colorPickerCode = colorPicker.replace(/[^a-zA-Z0-9 ]/g, '');
 
-fetch('https://www.thecolorapi.com/scheme?hex=ffd70d')
-  .then((res) => res.json())
-  .then((data) => {
-    data.colors.map((color, key) => {
-      colorBar[key].style.backgroundColor = color.hex.value;
-      colorCodes.innerHTML += `
+  fetch(`${baseURL}/scheme?hex=${colorPickerCode}`)
+    .then((res) => res.json())
+    .then((data) => {
+      data.colors.map((color, key) => {
+        colorBar[key].style.backgroundColor = color.hex.value;
+        colorCodes.innerHTML += `
       <p class="color-1">${color.hex.value}</p>
       `;
+      });
     });
-  });
+
+  colorCodes.innerHTML = '';
+});
